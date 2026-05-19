2f6994ea829849f242a846cc1e2f38a3b9221a21
Murodjon <desant.m91@gmail.com>
2026-05-19 20:29:07 +0500

    fix render token

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 
-BOT_TOKEN = os.getenv("8033895498:AAFSI-Yhp-pg3IBcpTrB_S2P9i7YfkOyMfA")
+BOT_TOKEN = os.environ.get("8033895498:AAFSI-Yhp-pg3IBcpTrB_S2P9i7YfkOyMfA")
 
 ADMIN_ID = 672575070
 
 PHONE_NUMBER = "+998999700091"
 
-BRANCH_ADDRESS = (
-    "Buloqboshi tumani Nayman markazi"
-)
+BRANCH_ADDRESS = "Buloqboshi tumani Nayman markazi"
 
 COURIERS = {
     "Ali": 5872585869
